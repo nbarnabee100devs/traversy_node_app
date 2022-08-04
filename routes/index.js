c8b02fd1a45115d36a56c165dd5a18f033eef738
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-08-04 16:50:46 +0200

    Add base routes

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+// The home for top-level routes
+
+const express = require("express");
+const router = express.Router();
+
+// Login/Landing page
+// GET /
+// Note that I must specify the layout, since I did not define it globally in app.js
+
+router.get("/", (req, res) => {
+  res.render("login.ejs", { layout: "./layouts/login" });
+});
+
+// Dashboard
+// GET /dashboard
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard.ejs", { layout: "./layouts/main" });
+});
+
+module.exports = router;
