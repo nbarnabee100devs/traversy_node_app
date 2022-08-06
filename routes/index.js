3a2e9c73ce7d5a992245385c86e1bbc98348ece6
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-08-06 15:16:45 +0200

    Add error pages and improved handling

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const mongoose = require("mongoose");
+const User = require("../models/User");
+const Story = require("../models/Story");
 
 // Login/Landing page
 // GET /
@@ -17,8 +20,22 @@ router.get("/", ensureGuest, (req, res) => {
 // GET /dashboard
 // We are including the ensureAuth middleware, which will redirect anyone who's not logged in from the dashboard to the login page (because people who aren't logged in should not be able to access the dashboard)
 
-router.get("/dashboard", ensureAuth, (req, res) => {
-  res.render("dashboard.ejs", { layout: "./layouts/main" });
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.user.id);
+    // with mongoose, .lean() will return an array of JSON objects
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard.ejs", {
+      layout: "./layouts/main",
+      name: currentUser.firstName,
+      stories,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render("error/500", {
+      layout: "./layouts/main",
+    });
+  }
 });
 
 module.exports = router;
