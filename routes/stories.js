71fe3ea05ff619c2a89dc5b3bf0f6ffa2fd587c5
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-08-08 11:47:34 +0200

    Add route

--- routes/stories.js
+++ routes/stories.js
@@ -14,4 +14,19 @@ router.get("/add", ensureAuth, (req, res) => {
   });
 });
 
+// Add route to POST a story (process the form)
+// POST /stories
+
+router.post("/", ensureAuth, async (req, res) => {
+  try {
+    // note that req.user returns an object containing the MongoDB ObjectId associated with said user
+    // we attach it to the req.body so that the two documents will be associated in the database
+    req.body.user = req.user.id;
+    await Story.create(req.body);
+    res.redirect("/dashboard");
+  } catch (error) {
+    res.render("error/500", { layout: "./layouts/main.ejs" });
+  }
+});
+
 module.exports = router;
