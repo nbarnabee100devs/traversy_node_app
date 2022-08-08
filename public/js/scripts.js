9f44c251418aefff63469d7ed7f5eaacc5b66a96
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-08-08 11:47:02 +0200

    Incorporate text editing library

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   M.Sidenav.init(document.querySelector(".sidenav"));
   M.FormSelect.init(document.getElementById("status"));
+  CKEDITOR.replace("body", {
+    plugins: "basicstyles, toolbar, wysiwygarea, link",
+  });
 });
